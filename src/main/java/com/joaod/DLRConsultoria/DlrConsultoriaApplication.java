d81d5d3e7fb43c5897276c73bccd9aceeb73add7
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-12-03 21:58:59 -0300

    Upload Project

---
+++ src/main/java/com/joaod/DLRConsultoria/DlrConsultoriaApplication.java
@@ -0,0 +1,13 @@
+package com.joaod.DLRConsultoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlrConsultoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlrConsultoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaod/DLRConsultoria/DlrConsultoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaod.DLRConsultoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlrConsultoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
